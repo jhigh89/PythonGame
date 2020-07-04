a7897f503f589c20cfe0c88543a61cd109794e02
Jacob Hightower <jah795@nau.edu>
2020-07-03 20:51:51 -0700

    added ufo pic, updated caption, added screenfill section

--- Main.py
+++ Main.py
@@ -6,9 +6,20 @@ pygame.init()
 # create screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Caption Icon displayed on screen
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+# loop for game
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+
+    # Fill background color of screen
+    screen.fill((0,0,0))
+    pygame.display.update()
+
+    
